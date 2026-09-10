d32927309aff6de3aab0e05163f7aa7de255c340
StudentHub <satpalnehra30@gmail.com>
2026-09-10 13:43:01 +0530

    Update vite.config.ts

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -66,6 +66,7 @@ function aistudioMediaPlugin(): Plugin {
 
 export default defineConfig(() => {
   return {
+     base: '/studenthubhelp/admin/',
     plugins: [react(), tailwindcss(), aistudioMediaPlugin()],
     resolve: {
       alias: {
